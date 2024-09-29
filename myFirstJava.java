7f75c6e1afe6234a2602b417d1a1fe1fe21be433
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 10:49:36 +0530

    changes without no

--- myFirstJava.java
+++ myFirstJava.java
@@ -1,6 +1,7 @@
 public class myFirstJava {
     public static void main(String[] args) {
-        System.out.println("my first java progrm");
+        System.out.println("my first \n java progrm");
+
     }
 }
 
