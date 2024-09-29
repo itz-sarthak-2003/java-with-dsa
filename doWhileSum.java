970b683845d4acd8475a4c9d0374d813bd991c1c
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 15:44:59 +0530

    successfully

---
+++ doWhileSum.java
@@ -0,0 +1,10 @@
+public class doWhileSum {
+    public static void main(String[] args) {
+        int i=0,sum=0;
+        do{
+            sum=sum+i;
+            i++;
+        }while(i<=5);
+        System.out.println(sum);
+    }
+}
