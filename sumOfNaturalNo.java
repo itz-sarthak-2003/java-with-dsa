0fd89ce70703826e7275d3a8f91857bcb0f318f1
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 15:35:29 +0530

    sum success

--- sumOfNaturalNo.java
+++ sumOfNaturalNo.java
@@ -1,5 +1,9 @@
 public class sumOfNaturalNo {
     public static void main(String[] args) {
-
+        int sum=0;
+    for(int i=0;i<=5;i++){
+       sum=sum+i;
+    }
+        System.out.println(sum);
     }
 }
