6d2735fdb28872dd91bfb1556ffce0f0c456ec12
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 11:10:56 +0530

    added successfylly

---
+++ elseIfCheckLessGreatEqual.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class elseIfCheckLessGreatEqual {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        if(a==b){
+            System.out.println("equal");
+        }else if (a<b){
+            System.out.println("a is less than b");
+    }else{
+            System.out.println("a is greate than b");
+}}}
