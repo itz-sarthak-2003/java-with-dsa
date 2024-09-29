6588dbecabc9aacd8c7d807e3392fb9c82a80a5d
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 11:02:03 +0530

    final update

--- checkLessEqualGreat.java
+++ checkLessEqualGreat.java
@@ -5,12 +5,12 @@ public class checkLessEqualGreat {
         int a=sc.nextInt();
         int b=sc.nextInt();
         if(a==b){
-            System.out.println("equal");
+            System.out.println("no. are equal");
         }else{
             if (a>b){
-                System.out.println("a is greater than b");
+                System.out.println(a+" is greater than "+b);
             }else {
-                System.out.println("a is lesser than b");
+                System.out.println(a+" is lesser than "+b);
             }
         }
     }
