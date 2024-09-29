6fe6057a50fd81632faf46f9e4cb668345c50bef
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-29 15:41:42 +0530

    sumWhile

---
+++ sumWhile.java
@@ -0,0 +1,10 @@
+public class sumWhile {
+    public static void main(String[] args) {
+        int i=0,sum=0;
+        while (i<=5){
+            sum=sum+i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
