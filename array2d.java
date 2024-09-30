f8b57f05f1d6e376c1a3ab7ab7f1ffbf77e2a50b
itz-sarthak-2003 <chaudharisarthak79@gmail.com>
2024-09-30 21:14:20 +0530

    succesfully run

--- array2d.java
+++ array2d.java
@@ -4,9 +4,9 @@ public class array2d {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter rows");
-        int rows=sc.nextInt();
-        System.out.println("enter cols");
         int cols=sc.nextInt();
+        System.out.println("enter cols");
+        int rows=sc.nextInt();
         int number[][]=new int[cols][rows];
         System.out.println("enter ele of array");
         for(int i=0;i<cols;i++){
@@ -17,8 +17,9 @@ public class array2d {
         System.out.println("u enetred ele");
         for(int i=0;i<cols;i++){
             for (int j=0;j<rows;j++){
-                System.out.println(number[i][j]);
+                System.out.print(number[i][j]+" ");
             }
+            System.out.println();
         }
     }
 }
